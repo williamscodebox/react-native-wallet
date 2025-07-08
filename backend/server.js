731097c75956e4a813d20c5ee1a5ab64ef699087
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-08 19:02:28 -0400

    added backend server

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("✅ Server is alive!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
