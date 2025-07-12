3e21b0fd03f90fb9b18ae6242e88ed1eb770b407
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-12 15:03:22 -0400

    updated index page

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,18 +1,11 @@
 import { Href, Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   const About = "/about" as Href;
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        // backgroundColor: "aqua",
-      }}
-    >
+    <View style={styles.container}>
       <Text style={{ color: "gray" }}>
         Edit app/index.tsx to edit this screen.
       </Text>
@@ -20,3 +13,12 @@ export default function Index() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: "aqua",
+  },
+});
