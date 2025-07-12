4046e2641088009918901f166c520998579c3fb9
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-11 20:38:13 -0400

    added about page

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,22 @@
+import { Href, Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const About = "/about" as Href;
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        // backgroundColor: "aqua",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ color: "gray" }}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+      <Link href={About}>About</Link>
     </View>
   );
 }
