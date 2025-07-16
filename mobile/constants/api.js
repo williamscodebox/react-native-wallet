a0c19ca82d571e26747c6bdfc9c5b09676144bd7
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-15 20:57:08 -0400

    updated api

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,2 @@
-//export const API_URL = "https://wallet-api-cxqp.onrender.com/api";
-export const API_URL = "http://localhost:5001/api";
+export const API_URL = "https://react-native-backend-iagl.onrender.com/api";
+//export const API_URL = "http://localhost:5001/api";
