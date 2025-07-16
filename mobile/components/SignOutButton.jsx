6fe24df324b7173dcdb48578cecbcb3cf30b2f8e
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-16 11:39:45 -0400

    updated SignOutButton

--- mobile/components/SignOutButton.jsx
+++ mobile/components/SignOutButton.jsx
@@ -1,6 +1,6 @@
 import { useClerk } from "@clerk/clerk-expo";
 import * as Linking from "expo-linking";
-import { Text, TouchableOpacity } from "react-native";
+import { Alert, Text, TouchableOpacity } from "react-native";
 import { styles } from "@/assets/styles/home.styles.js";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { COLORS } from "../constants/colors";
@@ -9,15 +9,19 @@ export const SignOutButton = () => {
   // Use `useClerk()` to access the `signOut()` function
   const { signOut } = useClerk();
   const handleSignOut = async () => {
-    try {
-      await signOut();
-      // Redirect to your desired page
-      Linking.openURL(Linking.createURL("/"));
-    } catch (err) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error(JSON.stringify(err, null, 2));
-    }
+    Alert.alert("Logout", "Are you sure you want to logout?", [
+      { text: "Cancel", style: "cancel" },
+      { text: "Logout", style: "destructive", onPress: signOut },
+    ]);
+    // try {
+    //   await signOut();
+    //   // Redirect to your desired page
+    //   Linking.openURL(Linking.createURL("/"));
+    // } catch (err) {
+    //   // See https://clerk.com/docs/custom-flows/error-handling
+    //   // for more info on error handling
+    //   console.error(JSON.stringify(err, null, 2));
+    // }
   };
   return (
     <TouchableOpacity style={styles.logoutButton} onPress={handleSignOut}>
